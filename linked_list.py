e4b6b09c588fc91036bd3cfe31f16dcf8a2aa4a6
vincentpham123 <vincentpham32@gmail.com>
2023-08-21 14:52:13 -0700

    more linked list practice

--- linked_list.py
+++ linked_list.py
@@ -25,3 +25,20 @@ def printList(head):
     printList(head.next)
 
 printList(a)
+
+def linked_list_values(head):
+  result=[]
+  current = head
+  while current is not None:
+    result.append(current.val)
+    current=current.next
+  return result
+
+
+def sum_list(head):
+  sum=0 
+  current=head 
+  while current is not None:
+    sum +=current.val 
+    current=current.next 
+  return sum 
