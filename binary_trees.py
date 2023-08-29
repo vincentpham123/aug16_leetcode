73ecc4ed0b56da6c436b80f9c2f09acf1029cb75
vincentpham123 <vincentpham32@gmail.com>
2023-08-29 11:19:35 -1000

    first binary tree practice

--- binary_trees.py
+++ binary_trees.py
@@ -13,3 +13,12 @@ def depth_first_values(root):
     if node.left:
       stack.append(node.left)
   return result
+
+def depth_first_values_recursive(root):
+  # will use a stack 
+  if not root:
+    return []
+  left_value = depth_first_values(root.left)
+  right_value= depth_first_values(root.right)
+  
+  return [root.val,*left_value,*right_value]
