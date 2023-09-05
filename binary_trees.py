0489190151ffc3b0f6e76c2b33ed9fb97feab40d
vincentpham123 <vincentpham32@gmail.com>
2023-09-04 20:31:48 -0700

    binary tree practice

--- binary_trees.py
+++ binary_trees.py
@@ -22,3 +22,60 @@ def depth_first_values_recursive(root):
   right_value= depth_first_values(root.right)
   
   return [root.val,*left_value,*right_value]
+
+def breadth_first_values(root):
+  # will use a queue
+  if root is None:
+    return []
+  queue=[root]
+  result=[]
+  
+  while queue:
+    node = queue.pop(0)
+    result.append(node.val)
+    if node.left:
+      queue.append(node.left)
+    if node.right:
+      queue.append(node.right)
+  return result
+
+
+def tree_includes(root, target):
+  stack=[root]
+  if not root:
+    return False
+  while stack:
+    node = stack.pop()
+    if node.val == target:
+      return True 
+    if node.left:
+      stack.append(node.left)
+    if node.right:
+      stack.append(node.right)
+  return False
+
+def tree_includes(root, target):
+  if not root:
+    return False
+  
+#   stack=[root]
+ 
+#   while stack:
+#     node = stack.pop()
+#     if node.val == target:
+#       return True 
+#     if node.left:
+#       stack.append(node.left)
+#     if node.right:
+#       stack.append(node.right)
+#   return False  
+  queue = [root]
+  while queue:
+    node = queue.pop(0)
+    if node.val == target:
+      return True 
+    if node.left:
+      queue.append(node.left)
+    if node.right:
+      queue.append(node.right)
+  return False
