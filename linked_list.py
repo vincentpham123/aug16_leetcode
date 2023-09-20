d5d10c9299f2d724dba073c1d46c07b35cc786b8
vincentpham123 <vincentpham32@gmail.com>
2023-09-20 10:52:14 -0700

    remove nth node

--- linked_list.py
+++ linked_list.py
@@ -217,3 +217,16 @@ def create_linked_list(values,i=0):
   head = Node(values[i])
   head.next = create_linked_list(values,i+1 )
   return head
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0,head)
+        right=head 
+
+        while n > 0:
+            right = right.next
+            n -= 1
+        while right:
+            left = left.next
+            right = right.next
+        left.next = left.next.next 
+        return dummy.next
+        
