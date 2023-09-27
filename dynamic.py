39f1d1a7d7d9e027ece7585ca4c415146d13abb3
vincentpham123 <vincentpham32@gmail.com>
2023-09-27 12:27:51 -0700

    dynamic sumpossible

--- dynamic.py
+++ dynamic.py
@@ -8,4 +8,24 @@ def _fib(n,memo):
     return memo[n]
   memo[n] = _fib(n - 1, memo) + _fib(n - 2, memo)
   return memo[n]
-  
+
+def sum_possible(amount, numbers, memo={}):
+    # will subtract from amount until it is too small
+    # i know if i am able to subtract the amount to zero, 
+    #using the numbers then sumpossible is true else false
+    if amount in memo:
+      return memo[amount]
+    if amount == 0:
+      return True
+    if amount < 0:
+      return False
+    
+    for num in numbers:
+      if sum_possible(amount-num, numbers) == True:
+        memo[amount] = True
+        return True
+    
+      # i need to check that true is returned from sumpossible
+      # it will recursivelysubtract from amount until it is 0 or less than 0
+    memo[amount] = False
+    return False  
