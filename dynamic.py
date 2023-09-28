ee0c5858adb46ffc718866b7110822dff7a7d177
vincentpham123 <vincentpham32@gmail.com>
2023-09-28 12:31:18 -0700

    more dyprog

--- dynamic.py
+++ dynamic.py
@@ -29,3 +29,40 @@ def sum_possible(amount, numbers, memo={}):
       # it will recursivelysubtract from amount until it is 0 or less than 0
     memo[amount] = False
     return False  
+
+def max_path_sum(grid):
+  return _max_path_sum(grid,0,0,{})
+def _max_path_sum(grid, r=0, c=0,memo={}):
+  pos = (r,c)
+  if pos in memo:
+    return memo[pos]
+  if r== len(grid) or c== len(grid[0]):
+    return float('-inf')
+  # returning negative infinity because it be compared with another sum
+  if r==len(grid)-1 and c== len(grid[0])-1:
+    return grid[r][c]
+  
+  down = _max_path_sum(grid,r+1,c)
+  right = _max_path_sum(grid,r,c+1)
+  
+  memo[pos] = grid[r][c] + max(down,right)
+  return memo[pos]
+
+def non_adjacent_sum(nums):
+  return _non_adjacent_sum(nums, 0, {})
+def _non_adjacent_sum(nums, i=0,memo={}):
+  if i in memo:
+    return memo[i]
+  if i >= len(nums):
+    return 0
+  
+  # exmaple 
+  # [2,3,5,12,7]
+  # when we call at i =0 (2)
+  # we need to choose if we are including 2 into the sum
+  
+  exclude = _non_adjacent_sum(nums, i+1)
+  include = nums[i] + _non_adjacent_sum(nums, i+2)
+  memo[i] = max(include, exclude)
+  
+  return memo[i]
