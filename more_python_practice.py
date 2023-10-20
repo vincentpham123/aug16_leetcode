1dc6c4e3375c774f314475922d252cb4e9610e1d
vincentpham123 <vincentpham32@gmail.com>
2023-10-20 12:56:20 -0700

    binary tree search

--- more_python_practice.py
+++ more_python_practice.py
@@ -5,13 +5,47 @@ def binary_search(numbers, target):
   midpoint = len(numbers)//2
   
   if numbers[midpoint] < target:
-    # result= binary_search(numbers[midpoint+1:],target)
-    # if result == -1:
-    #   return -1
-    # else:
-    #   return result + midpoint + 1
-    return binary_search(numbers[midpoint+1:],target)
+    result= binary_search(numbers[midpoint+1:],target)
+    if result == -1:
+      return -1
+    else:
+      return result + midpoint + 1
+ 
   elif numbers[midpoint] > target:
     return binary_search(numbers[:midpoint],target)
   else:
     return midpoint
+  
+def binary_search(numbers, target):
+  
+  
+  left = 0 
+  right = len(numbers)-1
+  
+  while left <= right:
+    midpoint = right+left //2 
+    
+    if numbers[midpoint] < target:
+      left = midpoint +1 
+    elif numbers[midpoint] > target:
+      right = midpoint-1 
+    else:
+      return midpoint
+  return -1
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def binary_search_tree_includes(root, target):
+  if root is None:
+    return False
+  if root.val == target:
+    return True 
+  
+  if root.val < target:
+      return binary_search_tree_includes(root.right, target)
+  else:
+      return binary_search_tree_includes(root.left, target)
