3c8b7fb063f8ae5e3831faeb2bca6f290e9def47
vincentpham123 <vincentpham32@gmail.com>
2023-12-15 10:17:43 -0800

    finished array review

--- dec_15.py
+++ dec_15.py
@@ -53,3 +53,64 @@ def char_count(s):
       count[char] = 0
     count[char] +=1 
   return count
+
+def pair_sum(numbers, target_sum):
+  
+  prev_nums = {}
+  
+  for index, num in enumerate(numbers):
+    other_sum = target_sum - num
+    
+    if other_sum in prev_nums:
+      return(index, prev_nums[other_sum])
+    # i will always record numbers in the dictionary, this way i have a linear look up time 
+    
+    prev_nums[num] = index
+
+def pair_product(numbers, target_product):
+  
+  previous_num = {}
+  
+  for index, num in enumerate(numbers):
+    other_num = target_product/num
+    
+    if other_num in previous_num:
+      return (index, previous_num[other_num])
+    previous_num[num] = index
+
+def intersection(a, b):
+  result_1 = set()
+  
+  result = []
+  
+  for num in a:
+    result_1.add(num)
+    
+  for num in b:
+    if num in result_1:
+      result.append(num)
+      
+  return result
+
+
+def five_sort(nums):
+  
+  i = 0 
+  j = len(nums) - 1
+  # i need i to check for 5s and J to keep track of numbers at the end that arent 5
+  # when i come across a 5 on index i, i switch with j 
+  # my conditions will check if j is not on a 5, and if i is on a 5 
+  # i will keep doing this until i and j meet 
+  
+  while i < j:
+    if nums[j]==5:
+      j-=1 
+    elif nums[i]!=5:
+      i+=1 
+    else:
+      # this condition will hit when i = 5
+      nums[i], nums[j] = nums[j], nums[i]
+      
+      i+=1 
+      
+  return nums
