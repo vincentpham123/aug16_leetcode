56c7ddf5feca5eb70c97f1877f9b60e3dfaacf8f
vincentpham123 <vincentpham32@gmail.com>
2024-01-05 15:36:50 -0800

    trib problem - performed hard and with memoization

--- jan_05.py
+++ jan_05.py
@@ -13,3 +13,17 @@ def _fib(n, memo):
   memo[n] = _fib(n-1, memo) + _fib(n-2, memo)
   
   return memo[n]
+
+def tribonacci(n):
+  memo = {}
+  return _trib(n, memo)
+
+def _trib(n, memo):
+  if n in memo:
+    return memo[n]
+  if n == 0 or n ==1:
+    return 0
+  if n ==2:
+    return 1 
+  memo[n] = _trib(n-1,memo) + _trib(n-2,memo) + _trib(n-3,memo)
+  return memo[n]
