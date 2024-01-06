63354af04f51a9b8224038ff94504980bfcd1d46
vincentpham123 <vincentpham32@gmail.com>
2024-01-06 13:14:53 -0800

    count paths problem

--- jan_05.py
+++ jan_05.py
@@ -68,4 +68,19 @@ def _min_change(amount, coins, memo):
     
   memo[amount] = size
   return size
+
+def count_paths(grid):
+  return _count_paths(grid, 0,0, {})
+
+def _count_paths(grid,r,c, memo):
   
+  pos = (r,c)
+  
+  if pos in memo: 
+    return memo[pos]
+  if r == len(grid) or c == len(grid[0]) or grid[r][c] == 'X':
+    return 0
+  if r== len(grid)-1 and c == len(grid[0]) -1:
+    return 1
+  memo[pos] = _count_paths(grid, r +1, c, memo) + _count_paths(grid, r,c+1,memo)
+  return memo[pos]
